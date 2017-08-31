9dd2fe196adf8e265383d6973300315e7546cffe
Yu Kitazume <u.kitazume@gmail.com>
2017-08-31 20:55:49 +0900

    not for after step

--- run.sh
+++ run.sh
@@ -7,29 +7,24 @@ if [ $DEPLOY != "true" ]; then
 fi
 
 # Configure Git.
-if [ "$WERCKER_RESULT" = "passed" ]; then
-    # Configure git
-    git config --global user.email pleasemailus@wercker.com
-    git config --global user.name "wercker"
-    debug 'configured git'
+git config --global user.email pleasemailus@wercker.com
+git config --global user.name "wercker"
+debug 'configured git'
 
-    # Get tags.
-    git fetch --tags $GIT_REMOTE
-    debug 'fetched git tags'
+# Get tags.
+git fetch --tags $GIT_REMOTE
+debug 'fetched git tags'
 
-    # Create the name of the tag
-    tagname="deploy-$WERCKER_GIT_COMMIT"
+# Create the name of the tag
+tagname="deploy-$WERCKER_GIT_COMMIT"
 
-    # Delete the tag if it exists, otherwise just skip
-    if (git tag -l | grep "$tagname" &> /dev/null);
-    then
-        git tag -d "$tagname"
-        debug 'Deleted existing $tagname'
-    fi
-
-    # Tag your commit.
-    git tag -a deploy-$WERCKER_GIT_COMMIT $WERCKER_GIT_COMMIT -m "Wercker deploy by $WERCKER_STARTED_BY :articulated_lorry:."
-    git push --tags $GIT_REMOTE
-else
-    info "skipping git tag because the deploy result is $WERCKER_RESULT"
+# Delete the tag if it exists, otherwise just skip
+if (git tag -l | grep "$tagname" &> /dev/null);
+then
+  git tag -d "$tagname"
+  debug 'Deleted existing $tagname'
 fi
+
+# Tag your commit.
+git tag -a deploy-$WERCKER_GIT_COMMIT $WERCKER_GIT_COMMIT -m "Wercker deploy by $WERCKER_STARTED_BY :articulated_lorry:."
+git push --tags $GIT_REMOTE
